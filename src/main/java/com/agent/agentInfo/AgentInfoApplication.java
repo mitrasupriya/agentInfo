d9ccc3603e1d854b7ba4447bdc3fd2dd282a894f
mitrasupriya <mitrasupriya3@gmail.com>
2020-12-04 00:59:01 +0530

    initial commit

---
+++ src/main/java/com/agent/agentInfo/AgentInfoApplication.java
@@ -0,0 +1,13 @@
+package com.agent.agentInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentInfoApplication.class, args);
+	}
+
+}
